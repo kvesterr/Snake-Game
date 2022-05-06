87d247538d3958db3b177c9eb1df81a7579f41e1
Daniil <56050759+kvesterr@users.noreply.github.com>
2022-05-06 17:03:11 +0300

    Update game.cpp
    Now game class is part of main window

--- game.cpp
+++ game.cpp
@@ -5,11 +5,9 @@
 //  Конструктор.
 Game::Game()
 {
+    this->setWindowFlags(Qt::Widget | Qt::FramelessWindowHint);
+
     resize(DOT_WIDTH * FIELD_WIDTH, DOT_HEIGHT * FIELD_HEIGHT);
-
-    setWindowTitle("Snake");
-
-    initGame();
 }
 
 //  Таймер (движение игрового процесса).
@@ -25,15 +23,6 @@ void Game::timerEvent(QTimerEvent *)
     this->repaint();  //  Перерисовать картинку.
 }
 
-//  Обработчик изменения направления змейки.
-void Game::keyPressEvent(QKeyEvent *event)
-{
-    int key = event->key();
-    if (key == Qt::Key_Left && m_dir != directions::right)  { m_dir = directions::left; }
-    if (key == Qt::Key_Right && m_dir != directions::left)  { m_dir = directions::right; }
-    if (key == Qt::Key_Up && m_dir != directions::down)     { m_dir = directions::up; }
-    if (key == Qt::Key_Down && m_dir != directions::up)     { m_dir = directions::down; }
-}
 
 //  Отрисовка.
 void Game::paintEvent(QPaintEvent *e)
@@ -65,13 +54,13 @@ void Game::doDrawing()
             else  //  Отрисовка хвоста
             {
                 qp.setBrush(Qt::green);
-                qp.drawEllipse(m_dots[i].x() * DOT_WIDTH, m_dots[i].y() * DOT_HEIGHT, DOT_WIDTH, DOT_HEIGHT);
+                qp.drawEllipse(m_dots[i].x() * DOT_WIDTH, m_dots[i].y()  * DOT_HEIGHT, DOT_WIDTH, DOT_HEIGHT);
             }
         }
     }
     else  //  Проигрышь.
     {
-        gameOver();
+        m_in_game = false;
     }
 }
 
@@ -81,8 +70,8 @@ void Game::localApple()
     QTime time = QTime::currentTime();
     qsrand((uint) time.msec());
 
-    m_apple.rx() = qrand() % DOT_WIDTH;
-    m_apple.ry() = qrand() % DOT_HEIGHT;
+    m_apple.rx() = qrand() % FIELD_WIDTH;
+    m_apple.ry() = qrand() % FIELD_HEIGHT;
 }
 
 //  Движение змейки.
@@ -98,10 +87,10 @@ void Game::move()
     //  В зависимости от направления меняем координаты.
     switch (m_dir)
     {
-    case left: {m_dots[0].rx() -= 1; break;}
-    case right: {m_dots[0].rx() += 1; break;}
-    case up: {m_dots[0].ry() -= 1; break;}
-    case down: {m_dots[0].ry() += 1; break;}
+    case 0:    {m_dots[0].ry() -= 1; break;}  //up
+    case 1:    {m_dots[0].rx() += 1; break;}  //right
+    case 2:    {m_dots[0].ry() += 1; break;}  //down
+    case 3:    {m_dots[0].rx() -= 1; break;}  //left
     }
 }
 
@@ -130,22 +119,13 @@ void Game::checkField()
     }
 }
 
-//  Функция проигрыша.
-void Game::gameOver()
-{
-    QMessageBox msgb;
-    msgb.setText("Game Over.");
-    msgb.exec();
-
-    initGame();
-}
-
 //  Проверка, съела ли змейка яблоко.
 void Game::checkApple()
 {
     if (m_apple == m_dots[0])
     {
         m_dots.push_back(QPoint(0, 0));
+        score++;
         localApple();
     }
 }
@@ -154,7 +134,8 @@ void Game::checkApple()
 void Game::initGame()
 {
     m_in_game = true;
-    m_dir = right;
+    m_dir = 1;
+    score = 0;
 
     m_dots.resize(3);
 
